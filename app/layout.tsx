8fad4a97dc407032a4bbb115fa74ea0c46dbd69d
Jorge Perez <information.jperez@gmail.com>
2025-05-17 10:59:58 +0800

    @vercel/analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'V0 Terminal - A Sleek Terminal-Inspired Developer Portfolio',
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className='antialiased'>
         {children}
       </body>
+      <Analytics/>
     </html>
   )
 }
